f3a7b4303783533f6c857944090e82c54ee204c3
Анастасія Кошуцька <zaporozhetsnastya89@gmail.com>
2026-09-14 21:36:30 +0300

    update searchBox: loader

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,4 +1,5 @@
 "use client";
+import SubmitButton from "../SubmitButton/SubmitButton";
 import css from "./SearchBox.module.css";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -6,35 +7,34 @@ export default function SearchBox() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const handleSearch = (formData: FormData) => {
-  const search = formData.get("search")?.toString() ?? "";
+    const search = formData.get("search")?.toString() ?? "";
 
-  const params = new URLSearchParams(searchParams.toString());
+    const params = new URLSearchParams(searchParams.toString());
 
-  params.delete("category");
+    params.delete("category");
 
-  if (search) {
-    params.set("search", search);
-  } else {
-    params.delete("search");
-  }
+    if (search) {
+      params.set("search", search);
+    } else {
+      params.delete("search");
+    }
 
-  params.set("page", "1");
+    params.set("page", "1");
 
-  router.push(`/recipes?${params.toString()}`);
-};
+    router.push(`/recipes?${params.toString()}`);
+  };
 
   return (
-<form action={handleSearch} className={css.form}>
-  <input
-    type="text"
-    name="search"
-    placeholder="Знайти рецепт або інгредієнт..."
-    className={css.input}
-  />
-
-  <button type="submit" className={css.button}>
-    Знайти
-  </button>
-</form>
+    <form action={handleSearch} className={css.form}>
+      <input
+        type="text"
+        name="search"
+        placeholder="Знайти рецепт або інгредієнт..."
+        className={css.input}
+      />
+      <SubmitButton pendingText="Знаходимо..." >
+        Знайти
+      </SubmitButton>
+    </form>
   );
 }
